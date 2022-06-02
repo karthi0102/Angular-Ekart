07321a59dc679d996034462d94d0e2cf95989631
karthi <karthirajendran12003@gmail.com>
2022-06-02 18:29:45 +0530

    form

--- server/models/Product.js
+++ server/models/Product.js
@@ -8,10 +8,6 @@ const productSchema = new Schema({
     // name:String
     name:String,
     price:Number,
-<<<<<<< HEAD
-    name:String,
-=======
->>>>>>> 86d2aee63bf0871cc1ee13d4b1afc290d9a2c941
     rating:Number,
     image:String
 })
